b7bfd9a8787df9f5b0a04c537800bf6995997594
gunkim <gunkim.dev@gmail.com>
2022-10-02 01:13:36 +0900

    :heavy_plus_sign: 프로젝트 개발을 위한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,22 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
+    id("org.springframework.boot") version "2.4.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 group = "io.github.gunkim"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter:2.7.4")
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    implementation("net.dv8tion:JDA:5.0.0-alpha.20")
 }
 
 tasks.test {
@@ -20,5 +25,5 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
 }
