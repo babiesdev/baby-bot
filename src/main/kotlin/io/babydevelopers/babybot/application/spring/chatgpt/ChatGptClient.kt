6fe08ec3745b004213148b116f34d02abab62a31
gunkim <gunkim.dev@gmail.com>
2023-02-20 22:05:28 +0900

    :sparkles: 멘션 이벤트에 대해 Chat-Gpt 응답 구현

---
+++ src/main/kotlin/io/babydevelopers/babybot/application/spring/chatgpt/ChatGptClient.kt
@@ -0,0 +1,17 @@
+package io.babydevelopers.babybot.application.spring.chatgpt
+
+import io.babydevelopers.babybot.application.spring.chatgpt.model.ChatGptRequest
+import io.babydevelopers.babybot.application.spring.chatgpt.model.ChatGptResponse
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.service.annotation.HttpExchange
+import org.springframework.web.service.annotation.PostExchange
+import reactor.core.publisher.Mono
+
+@HttpExchange(
+    accept = ["application/json"],
+    contentType = "application/json"
+)
+interface ChatGptClient {
+    @PostExchange("/v1/completions")
+    fun sendMessage(@RequestBody requestParam: ChatGptRequest): Mono<ChatGptResponse>
+}
