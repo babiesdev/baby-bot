9f3f34254beb37afb3f7cabf90065ee03a1ca3ac
Mang-Joo <wjwan0915@gmail.com>
2023-03-03 02:03:05 +0900

    :sparkles: 승인 개발 중

--- src/main/kotlin/io/babydevelopers/babybot/application/spring/discord/listener/CommandStudyController.kt
+++ src/main/kotlin/io/babydevelopers/babybot/application/spring/discord/listener/CommandStudyController.kt
@@ -2,8 +2,10 @@ package io.babydevelopers.babybot.application.spring.discord.listener
 
 import io.babydevelopers.babybot.application.spring.discord.model.StudySlashCommand
 import io.babydevelopers.babybot.application.spring.discord.model.StudySlashCommand.ADMISSION
+import io.babydevelopers.babybot.application.spring.discord.model.StudySlashCommand.APPROVAL
 import io.babydevelopers.babybot.application.spring.discord.model.StudySlashCommand.DELETE
 import io.babydevelopers.babybot.application.spring.discord.model.StudySlashCommand.ENTER
+import io.babydevelopers.babybot.application.spring.discord.service.ManualSutdyService
 import net.dv8tion.jda.api.Permission.ADMINISTRATOR
 import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
@@ -18,7 +20,7 @@ private val SlashCommandInteractionEvent.hasAdminRole: Boolean
 
 @Controller
 class CommandStudyController(
-    private val manualForumController: ManualForumController,
+    private val manualSutdyService: ManualSutdyService,
 ) : ListenerAdapter() {
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
         when (StudySlashCommand.from(event.name)) {
@@ -27,11 +29,19 @@ class CommandStudyController(
             }
 
             DELETE -> runWithAdminPrivileges(event) {
-                manualForumController.onChannelDelete(event)
+                manualSutdyService.onChannelDelete(event)
                 event.sendMessage("보이스 채널을 삭제하였습니다.")
             }
 
-            ENTER -> enterChannel(event)
+            ENTER -> {
+                manualSutdyService.onChannelEnterRequest(event)
+                event.sendMessage("${event._member.effectiveName}님이 ${event.channel.name} 스터디에 참여하였습니다.")
+            }
+
+            APPROVAL -> runWithAdminPrivileges(event) {
+                manualSutdyService.onChannelEnterApproval(event)
+                event.sendMessage("승인이 완료되었습니다.")
+            }
         }
     }
 
@@ -43,18 +53,9 @@ class CommandStudyController(
         action()
     }
 
-    private fun enterChannel(event: SlashCommandInteractionEvent) {
-        try {
-            manualForumController.onChannelEnter(event)
-            event.sendMessage("${event._member.effectiveName}님이 ${event.channel.name} 스터디에 참여하였습니다.")
-        } catch (e: IllegalArgumentException) {
-            event.sendMessage(e.message.toString())
-        }
-    }
-
     private fun createChannel(event: SlashCommandInteractionEvent) {
         try {
-            manualForumController.onChannelCreate(event)
+            manualSutdyService.onChannelCreate(event)
             event.sendMessage("보이스 채널을 생성하였습니다.")
         } catch (e: IllegalArgumentException) {
             event.sendMessage(e.message.toString())
