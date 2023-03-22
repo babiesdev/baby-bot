28e751dba87610c1eeb11e6b77c33ee52fbd8667
junnyjun <chbe5082@naver.com>
2023-03-22 14:18:03 +0900

    feat(monitor) : fix gradle conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ repositories {
 }
 
 dependencies {
-    implementation 'org.springframework.boot:spring-boot-starter-actuator'
-    implementation 'io.micrometer:micrometer-registry-prometheus'
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
