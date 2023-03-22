8c896c07d73d0080270ddcb3f7a46eae21f2ac35
junnyjun <chbe5082@naver.com>
2023-03-22 14:17:25 +0900

    feat(monitor) : add metric

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,9 @@ repositories {
 }
 
 dependencies {
+    implementation 'org.springframework.boot:spring-boot-starter-actuator'
+    implementation 'io.micrometer:micrometer-registry-prometheus'
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-test")
