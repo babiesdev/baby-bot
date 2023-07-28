9f5752c5994d989bd4a76df82c27988629a6f915
gunkim <gunkim.dev@gmail.com>
2023-07-29 00:11:44 +0900

    :fire: 불필요한 컴포넌트 스캔 어노테이션 제거

--- src/main/kotlin/io/babydevelopers/babybot/application/spring/Application.kt
+++ src/main/kotlin/io/babydevelopers/babybot/application/spring/Application.kt
@@ -2,11 +2,8 @@ package io.babydevelopers.babybot.application.spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan("io.babydevelopers.babybot.application.spring.discord")
-@ComponentScan("io.babydevelopers.babybot.application.spring.config")
 class Application
 
 fun main(args: Array<String>) {
