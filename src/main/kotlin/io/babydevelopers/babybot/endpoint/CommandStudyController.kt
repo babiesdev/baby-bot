66f4fd4f995da10dd2d07b3cc10031d753562bfa
gunkim <gunkim.dev@gmail.com>
2023-07-29 00:28:31 +0900

    :fire: 불필요 파일 제거

--- src/main/kotlin/io/babydevelopers/babybot/endpoint/CommandStudyController.kt
+++
@@ -1,6 +0,0 @@
-package io.babydevelopers.babybot.endpoint
-
-import net.dv8tion.jda.api.Permission
-import net.dv8tion.jda.api.entities.Member
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-
